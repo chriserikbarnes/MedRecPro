732d1ceba2c48d0fe37be1877e759ed4e5843a95
Chris Barnes <chris.erik.barnes@gmail.com>
2026-02-23 12:23:50 -0500

    Moved file into folder and renamed namespace to mactch the others in this folder

--- MedRecProImportClass/Service/ParsingServices/OrangeBookProductParsingService.cs
+++ MedRecProImportClass/Service/ParsingServices/OrangeBookProductParsingService.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Logging;
 using System.Globalization;
 using static MedRecProImportClass.Models.Label;
 
-namespace MedRecProImportClass.Service
+namespace MedRecProImportClass.Service.ParsingServices
 {
     /*************************************************************/
     /// <summary>
